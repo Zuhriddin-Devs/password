eb1dfe20cb2d3d7f55501adb82a96aba8fdaf318
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-12-22 13:49:52 +0500

    The site has been locked...

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const request = prompt('Password: ********');
 
 const access = () => {
-
+    if (request == 12345678) {
+        alert(`:)`);
+        window.open();
+    } else {
+        alert(`:(`);
+        window.close();
+    };
 };
 
 access();
